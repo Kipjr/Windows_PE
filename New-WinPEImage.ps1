18645dec14006acdaa4d45efc5d298c9866dae40
Kipjr <12066560+Kipjr@users.noreply.github.com>
2022-04-18 17:01:32 +0200

    Fix: Typos

--- New-WinPEImage.ps1
+++ New-WinPEImage.ps1
@@ -2,13 +2,12 @@
 #https://go.microsoft.com/fwlink/?linkid=2166133 #ADK Add-on WinPE
 #https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/winpe-mount-and-customize?view=windows-11
 
-set-location ${Env:WORKING-DIRECTORY}
-$json=get-content -path ./env.json -raw | convertfrom-json
+$json=get-content -path .\env.json -raw | convertfrom-json
 $dismloc="$env:systemroot\System32\Dism.exe"
 
 function DismAddPackage {
     Param(
-        [string]$image="${Env:WORKING-DIRECTORY}\WinPE_amd64\mount",
+        [string]$image=".\WinPE_amd64\mount",
         [Parameter(Mandatory=$true)][string]$packageName
     )
     
@@ -23,7 +22,7 @@ function DismAddPackage {
         exit 1
     }
     
-    $command1="$dismloc /Mount-Image /ImageFile`:$image /PackagePath:$package"
+    $command1="$dismloc /Mount-Image /ImageFile`:$image /PackagePath:$packagePath"
     $command2="$dismloc /Mount-Image /ImageFile`:$image /PackagePath:$LangpackagePath"
 
     & $command1
@@ -32,10 +31,10 @@ function DismAddPackage {
 
 
 #Start the Deployment and Imaging Tools Environment as an administrator.
-copype amd64 C:\WinPE_amd64
+copype amd64 .\WinPE_amd64
 
 "Mounting image" | write-host -foregroundcolor magenta
-& "$env:systemroot\System32\Dism.exe" /Mount-Image /ImageFile:"${Env:WORKING-DIRECTORY}\WinPE_amd64\media\sources\boot.wim" /index:1 /MountDir:"C:\WinPE_amd64\mount"
+& "$env:systemroot\System32\Dism.exe" /Mount-Image /ImageFile:".\WinPE_amd64\media\sources\boot.wim" /index:1 /MountDir:".\WinPE_amd64\mount"
 
 "Adding Optional Components" | write-host -foregroundcolor magenta
 foreach($c in $json.WinPEOptionalComponents){
@@ -44,7 +43,7 @@ foreach($c in $json.WinPEOptionalComponents){
 }
 
 "Unmounting image" | write-host -foregroundcolor magenta
-& "$env:systemroot\System32\Dism.exe" /Unmount-Image /MountDir:"${Env:WORKING-DIRECTORY}\WinPE_amd64\mount" /commit
+& "$env:systemroot\System32\Dism.exe" /Unmount-Image /MountDir:".\WinPE_amd64\mount" /commit
 
 "Create ISO file" | write-host -foregroundcolor magenta
-MakeWinPEMedia /ISO "${Env:WORKING-DIRECTORY}\WinPE_amd64" "${Env:WORKING-DIRECTORY}\WinPE_amd64.iso"
+MakeWinPEMedia /ISO ".\WinPE_amd64" ".\WinPE_amd64.iso"
