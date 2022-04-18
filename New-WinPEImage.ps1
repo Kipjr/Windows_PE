dcca824f3b3e44415f992ab83bd9e8c8ee5e1cb6
Kipjr <12066560+Kipjr@users.noreply.github.com>
2022-04-18 23:11:54 +0200

    Add: Paths & ENVs

--- New-WinPEImage.ps1
+++ New-WinPEImage.ps1
@@ -3,50 +3,52 @@
 #https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/winpe-mount-and-customize?view=windows-11
 
 $json=get-content -path .\env.json -raw | convertfrom-json
-$dismloc="$env:systemroot\System32\Dism.exe"
+$adkPATH="C:\Program Files\Windows Kits\10\Assessment and Deployment Kit"
+$WinPEPATH="$adkPATH\Windows Preinstallation Environment"
+$WinPEOCPath="$WinPEPATH\amd64\WinPE_OCs"
+
+$dismPATH="$env:systemroot\System32\Dism.exe" 
+$DeployImagingToolsENV="$adkPATH\Deployment Tools\DandISetEnv.bat" #Deployment and Imaging Tools Environment
 
 function DismAddPackage {
     Param(
         [string]$image=".\WinPE_amd64\mount",
         [Parameter(Mandatory=$true)][string]$packageName
     )
-    
-    $WinPEPath="C:\Program Files\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs"
-    
     try {
-        $packagePath = if(test-path "$WinPEPath\$packageName.cab") { $="$WinPEPath\$packageName.cab" } else {$null}
-        $LangpackagePath = if(test-path "$WinPEPath\en-us\$packageName.cab") { "$WinPEPath\en-us\$packageName`_en-us.cab" } else {$null}
+        $packagePath = if(test-path "$WinPEPath\$packageName.cab") { $="$WinPEOCPath\$packageName.cab" } else {$null}
+        $LangpackagePath = if(test-path "$WinPEPath\en-us\$packageName.cab") { "$WinPEOCPath\en-us\$packageName`_en-us.cab" } else {$null}
     }
     catch {
         write-host "Issue with $packageName.cab"| write-host -foregroundcolor red
         exit 1
     }
     
-    $command1="$dismloc /Mount-Image /ImageFile`:$image /PackagePath:$packagePath"
-    $command2="$dismloc /Mount-Image /ImageFile`:$image /PackagePath:$LangpackagePath"
+    $command1="$dismPATH /Mount-Image /ImageFile`:$image /PackagePath:$packagePath"
+    $command2="$dismPATH /Mount-Image /ImageFile`:$image /PackagePath:$LangpackagePath"
 
     & $command1
     & $command2
 }
 
 try {
-#Start the Deployment and Imaging Tools Environment as an administrator.
-& "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\copype.cmd" amd64 .\WinPE_amd64
+    "Start the Deployment and Imaging Tools Environment & create winpe for amd64" | write-host -foregroundcolor magenta
+    & "cmd /c `"$DeployImagingToolsENV`" && `"$WinPEPATH\copype.cmd`" amd64 .\WinPE_amd64"
 
-"Mounting image" | write-host -foregroundcolor magenta
-& "$env:systemroot\System32\Dism.exe" /Mount-Image /ImageFile:".\WinPE_amd64\media\sources\boot.wim" /index:1 /MountDir:".\WinPE_amd64\mount"
+    "Mounting image" | write-host -foregroundcolor magenta
+    & "$env:systemroot\System32\Dism.exe" /Mount-Image /ImageFile:".\WinPE_amd64\media\sources\boot.wim" /index:1 /MountDir:".\WinPE_amd64\mount"
 
-"Adding Optional Components" | write-host -foregroundcolor magenta
-foreach($c in $json.WinPEOptionalComponents){
-    "Adding: $c" | write-host -foregroundcolor cyan
-    DismAddPackage -packageName $c
-}
+    "Adding Optional Components" | write-host -foregroundcolor magenta
+    foreach($c in $json.WinPEOptionalComponents){
+        "Adding: $c" | write-host -foregroundcolor cyan
+        DismAddPackage -packageName $c
+    }
 
-"Unmounting image" | write-host -foregroundcolor magenta
-& "$env:systemroot\System32\Dism.exe" /Unmount-Image /MountDir:".\WinPE_amd64\mount" /commit
+    "Unmounting image" | write-host -foregroundcolor magenta
+    & "$env:systemroot\System32\Dism.exe" /Unmount-Image /MountDir:".\WinPE_amd64\mount" /commit
 
-"Create ISO file" | write-host -foregroundcolor magenta
-& "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\makeWinPEMedia.cmd" /ISO ".\WinPE_amd64" ".\WinPE_amd64.iso"
+    "Start the Deployment and Imaging Tools Environment & Create ISO file" | write-host -foregroundcolor magenta
+    & "cmd /c `"$DeployImagingToolsENV`" && `"$WinPEPATH\makeWinPEMedia.cmd`" /ISO .\WinPE_amd64 .\WinPE_amd64.iso"
 } catch {
-  get-content -path "C:\Windows\Logs\DISM\dism.log"
+    get-content -path "C:\Windows\Logs\DISM\dism.log"
 }
