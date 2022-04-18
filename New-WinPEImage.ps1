c50d7a50863cf9f9ddb4966cc4894315bbcc5556
Kipjr <12066560+Kipjr@users.noreply.github.com>
2022-04-19 00:16:16 +0200

    Fix: CMD env var

--- New-WinPEImage.ps1
+++ New-WinPEImage.ps1
@@ -3,7 +3,7 @@
 #https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/winpe-mount-and-customize?view=windows-11
 
 $json=get-content -path .\env.json -raw | convertfrom-json
-$adkPATH="C:\Program Files\Windows Kits\10\Assessment and Deployment Kit"
+$adkPATH="C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit"
 $WinPEPATH="$adkPATH\Windows Preinstallation Environment"
 $WinPEOCPath="$WinPEPATH\amd64\WinPE_OCs"
 
@@ -33,10 +33,11 @@ function DismAddPackage {
 
 try {
     "Start the Deployment and Imaging Tools Environment & create winpe for amd64" | write-host -foregroundcolor magenta
-    & cmd /c `"$DeployImagingToolsENV`" && `"$WinPEPATH\copype.cmd`" amd64 .\WinPE_amd64
+    cmd /k """$DeployImagingToolsENV"" && copype.cmd amd64 %GITHUB_WORKSPACE%\WinPE_amd64 && exit"
+    
 
     "Mounting image" | write-host -foregroundcolor magenta
-    & "$env:systemroot\System32\Dism.exe" /Mount-Image /ImageFile:".\WinPE_amd64\media\sources\boot.wim" /index:1 /MountDir:".\WinPE_amd64\mount"
+    & "$env:systemroot\System32\Dism.exe" /Mount-Image /ImageFile:"$env:GITHUB_WORKSPACE\WinPE_amd64\media\sources\boot.wim" /index:1 /MountDir:"$env:GITHUB_WORKSPACE\WinPE_amd64\mount"
 
     "Adding Optional Components" | write-host -foregroundcolor magenta
     foreach($c in $json.WinPEOptionalComponents){
@@ -45,10 +46,10 @@ try {
     }
 
     "Unmounting image" | write-host -foregroundcolor magenta
-    & "$env:systemroot\System32\Dism.exe" /Unmount-Image /MountDir:".\WinPE_amd64\mount" /commit
+    & "$env:systemroot\System32\Dism.exe" /Unmount-Image /MountDir:"$env:GITHUB_WORKSPACE\WinPE_amd64\mount" /commit
 
     "Start the Deployment and Imaging Tools Environment & Create ISO file" | write-host -foregroundcolor magenta
-    & cmd /c `"$DeployImagingToolsENV`" && `"$WinPEPATH\makeWinPEMedia.cmd`" /ISO .\WinPE_amd64 .\WinPE_amd64.iso
+    cmd /k """$DeployImagingToolsENV"" && makeWinPEMedia.cmd /ISO %GITHUB_WORKSPACE	%\WinPE_amd64 %GITHUB_WORKSPACE	%\WinPE_amd64.iso && exit"
 } catch {
     get-content -path "C:\Windows\Logs\DISM\dism.log" | Where-Object {$_ -like "$(get-date -f 'yyyy-MM-dd')*"} | Select-Object -Last 250
 }
