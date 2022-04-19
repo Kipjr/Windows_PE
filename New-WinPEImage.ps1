2315199571569ec1c090104853c3cc710cda3c1f
Kipjr <12066560+Kipjr@users.noreply.github.com>
2022-04-19 21:36:01 +0200

    Fix: Files & Folders + Drivers

--- New-WinPEImage.ps1
+++ New-WinPEImage.ps1
@@ -6,56 +6,104 @@
 $json=get-content -path .\env.json -raw | convertfrom-json
 $adkPATH="C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit"
 $WinPEPATH="$adkPATH\Windows Preinstallation Environment"
-$WinPEOCPath="$WinPEPATH\amd64\WinPE_OCs"
-
 $DeployImagingToolsENV="$adkPATH\Deployment Tools\DandISetEnv.bat" #Deployment and Imaging Tools Environment
 
-
-
-"Start the Deployment and Imaging Tools Environment & create winpe for amd64" | write-host -foregroundcolor magenta
+<# 
+    Creation of WinPE
+ #>
+ 
+"Start the Deployment and Imaging Tools Environment & Create WinPE for amd64" | write-host -foregroundcolor magenta
 cmd /k """$DeployImagingToolsENV"" && copype.cmd amd64 %GITHUB_WORKSPACE%\WinPE_amd64 && exit"
 
-
-"Mounting image" | write-host -foregroundcolor magenta
+"Mounting boot.wim image" | write-host -foregroundcolor magenta
 Mount-WindowsImage -ImagePath "$env:GITHUB_WORKSPACE\WinPE_amd64\media\sources\boot.wim" -index 1  -Path "$env:GITHUB_WORKSPACE\WinPE_amd64\mount"
+$WinPE_root = "$env:GITHUB_WORKSPACE\WinPE_amd64\mount"
 
-
-"Adding Optional Components" | write-host -foregroundcolor magenta
+<# 
+    Optional Components
+ #>
+ 
+"Adding Optional Components to boot.wim" | write-host -foregroundcolor magenta
 foreach($c in $json.WinPEOptionalComponents){
     "Adding: $c" | write-host -foregroundcolor cyan
-    Add-WindowsPackage -Path "$env:GITHUB_WORKSPACE\WinPE_amd64\mount" -PackagePath "$WinPEOCPath\$c.cab" -PreventPending
-    if(test-path -path "$WinPEOCPath\en-us\$c`_en-us.cab" ){
-        Add-WindowsPackage -Path "$env:GITHUB_WORKSPACE\WinPE_amd64\mount" -PackagePath "$WinPEOCPath\en-us\$c`_en-us.cab" -PreventPending
+    Add-WindowsPackage -Path "$env:GITHUB_WORKSPACE\WinPE_amd64\mount" -PackagePath "$WinPEPATH\amd64\WinPE_OCs\$c.cab" -PreventPending
+    if(test-path -path "$WinPEPATH\amd64\WinPE_OCs\en-us\$c`_en-us.cab" ){
+        Add-WindowsPackage -Path "$env:GITHUB_WORKSPACE\WinPE_amd64\mount" -PackagePath "$WinPEPATH\amd64\WinPE_OCs\en-us\$c`_en-us.cab" -PreventPending
     } else {
      "$c`_en-us.cab not found.. continuing" | write-host -foregroundcolor cyan
     }
 }
 
-<#     "Adding lines to winpeshl.ini" | write-host -ForegroundColor magenta
-"[LaunchApps]" | Out-File -FilePath .\winpeshl.ini
-
+<# 
+    Default Start commands
+ #>
+ 
+"Adding lines to winpeshl.ini" | write-host -ForegroundColor magenta
+"[LaunchApps]" | Out-File -FilePath .\winpeshl.ini #write to local file
 foreach($p in $json.winpeshl_commands){
-    "$c" | write-host -ForegroundColor cyan
+    "$p" | write-host -ForegroundColor cyan
     #run in order of appearance, and don’t start until the previous app has terminated.
     #[LaunchApps]
-    "$c"  | Out-File -FilePath .\winpeshl.ini
+    "$p"  | Out-File -append -FilePath .\winpeshl.ini
     #%SYSTEMROOT%\System32\bddrun.exe /bootstrap
 }
-"%SYSTEMROOT%\System32\bddrun.exe, /bootstrap" | Out-File -FilePath .\winpeshl.ini
-rename-item "$env:GITHUB_WORKSPACE\WinPE_amd64\mount\windows\system32\winpeshl.ini" "$env:GITHUB_WORKSPACE\WinPE_amd64\mount\windows\system32\winpeshl.ini.old"
-copy-item ./winpeshl.ini "$env:GITHUB_WORKSPACE\WinPE_amd64\mount\windows\system32\winpeshl.ini" #>
+"%SYSTEMROOT%\System32\bddrun.exe, /bootstrap" | Out-File -append -FilePath .\winpeshl.ini
+if(test-path -path "$env:GITHUB_WORKSPACE\WinPE_amd64\mount\windows\system32\winpeshl.ini") {
+    rename-item "$env:GITHUB_WORKSPACE\WinPE_amd64\mount\windows\system32\winpeshl.ini" "$env:GITHUB_WORKSPACE\WinPE_amd64\mount\windows\system32\winpeshl.ini.old"
+}
+copy-item ./winpeshl.ini "$env:GITHUB_WORKSPACE\WinPE_amd64\mount\windows\system32\winpeshl.ini"
 
-<#     "Adding drivers" | write-host -ForegroundColor magenta    
+<# 
+    Add files & folders
+ #>
+ 
+"Adding Files & Folders" | write-host -ForegroundColor magenta
+$oldloc=get-location 
+set-location .\source\_winpe
+$folders = get-childitem -directory -Path "." -Recurse |  where {$_.FullName -notlike "*.ignore*"}  | Resolve-Path -Relative
+$files = get-childitem -file -Path "." -Recurse |  where {$_.FullName -notlike "*.ignore*"}   | Resolve-Path -Relative
+
+foreach($fo in $folders) {
+    if(!(test-path -path "$WinPE_root\$fo")){
+        New-Item -ItemType Directory "$WinPE_root\$fo" -verbose
+    }
+}
+foreach($fi in $files) {
+        copy-item -path  "$fi" -destination "$WinPE_root\$fi" -verbose
+}
+Set-Location $oldloc
+
+<# 
+    Add Drivers
+ #>
+ 
+"Adding drivers" | write-host -ForegroundColor magenta    
 foreach($b in $json.bootdrivers){
     "$b" | write-host -ForegroundColor cyan
-    Add-WindowsDriver -Path "$env:GITHUB_WORKSPACE\WinPE_amd64\mount" -Driver "$b" -ForceUnsigned
+    $infitem = get-childitem $json.bootdrivers -Recurse  -Filter "*.inf" | select -ExpandProperty FullName
+    foreach($i in $infitem){
+        if(test-path -path $i) {
+            Add-WindowsDriver -Path "$env:GITHUB_WORKSPACE\WinPE_amd64\mount" -Driver "$i" -ForceUnsigned
+        }
+    }
 }
-#>
-"Unmounting image" | write-host -foregroundcolor magenta
+
+<# 
+    Generating hash of contents
+ #>
+ 
+"Generating hash of contents boot.wim " | write-host -foregroundcolor magenta
+ get-childitem "$env:GITHUB_WORKSPACE\WinPE_amd64\mount" -Recurse -File | select @{n="File";e={$_.Fullname| Resolve-Path -Relative }}, @{n="SHA256_filehash";e={ ($_.fullname | Get-FileHash -Algorithm SHA256).hash }} | Export-Csv -Path .\filelist_boot.wim.csv -Delimiter ";"
+
+<# 
+    Umounting & create iso
+ #>
+ 
+"Unmounting boot.wim image" | write-host -foregroundcolor magenta
 Dismount-WindowsImage -Path "$env:GITHUB_WORKSPACE\WinPE_amd64\mount" -Save
 
-"Start the Deployment and Imaging Tools Environment & Create ISO file" | write-host -foregroundcolor magenta
+"Start the Deployment and Imaging Tools Environment & Create ISO file from mount folder" | write-host -foregroundcolor magenta
 cmd /k """$DeployImagingToolsENV"" && makeWinPEMedia.cmd /ISO %GITHUB_WORKSPACE%\WinPE_amd64 %GITHUB_WORKSPACE%\WinPE_amd64.iso && exit"
 
-#   get-content -path "C:\Windows\Logs\DISM\dism.log" | Where-Object {$_ -like "$(get-date -f 'yyyy-MM-dd')*"} | Select-Object -Last 250
+# get-content -path "C:\Windows\Logs\DISM\dism.log" | Where-Object {$_ -like "$(get-date -f 'yyyy-MM-dd')*"} | Select-Object -Last 250
 
